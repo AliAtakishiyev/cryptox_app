7fa01afec5d4fb793e14b24d588acf09ee7eaa17
aliatakishiyev <aliatakishiyev2006@gmail.com>
2025-12-24 18:04:33 +0300

    feat: Network layer

---
+++ lib/features/core/network/dio_client.dart
@@ -0,0 +1,26 @@
+import 'package:cryptox_app/features/core/network/api_endpoints.dart';
+import 'package:dio/dio.dart';
+
+class DioClient {
+  late final Dio dio;
+
+  DioClient() {
+    dio = Dio(
+      BaseOptions(
+        baseUrl: ApiEndpoints.baseUrl,
+        connectTimeout: const Duration(seconds: 10),
+        receiveTimeout: const Duration(seconds: 10),
+        headers: {'Content-Type': 'application/json'},
+      ),
+    );
+
+    dio.interceptors.add(
+      LogInterceptor(
+        request: true,
+        error: true,
+        responseBody: true,
+        requestBody: true,
+      ),
+    );
+  }
+}
